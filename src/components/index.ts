37a07ddfada6b827cb5d57388b33343028bee14d
min-s9709 <mim970929@naver.com>
2025-01-11 20:18:07 +0900

    add: SearchBar 관련 svg 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as SearchBar } from './common/SearchBar'
 export { default as Footer } from './layouts/Footer'
 export { default as Header } from './layouts/Header'
 export { default as Layout } from './layouts/Layout'
