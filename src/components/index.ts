a7f2ae19164bc8b00a9bd0dadd3fe6d8e88a2a92
beom <bdh3659@naver.com>
2025-01-12 02:28:24 +0900

    chore: 구현한 컴포넌트 index에 추가

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as CommentInput } from './common/CommentInput'
+export { default as ContentInput } from './common/ContentInput'
+export { default as TitleInput } from './common/TitleInput'
