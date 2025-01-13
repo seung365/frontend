88b37afecc7dd12b04563dc8284710c708134247
beom <bdh3659@naver.com>
2025-01-13 15:41:39 +0900

    feat: Button 컴포넌트 구현

---
+++ src/components/common/Button/index.tsx
@@ -0,0 +1,46 @@
+import { ButtonHTMLAttributes, ReactNode } from 'react'
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode
+  size?: 'small' | 'medium' | 'large'
+  theme?: 'light' | 'dark'
+  onClick?: () => void
+}
+
+/*
+  Button
+  Description:
+    - 버튼 컴포넌트
+    - children, size, theme, onClick을 props로 받아서 사용
+*/
+
+const sizeClasses = {
+  small: 'h-8 px-3 text-sm  text-sm rounded-md',
+  medium: 'h-10 px-4 text-base rounded-lg',
+  large: 'h-12 px-5 text-xl rounded-xl',
+}
+
+const themeClasses = {
+  dark: 'bg-main-color text-white',
+  light: 'bg-sub-color text-black',
+}
+
+const Button = ({
+  children,
+  size = 'medium',
+  theme = 'dark',
+  onClick,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`font-medium inline-flex items-center justify-center  ${sizeClasses[size]} ${themeClasses[theme]} border-solid focus:outline-none`}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
