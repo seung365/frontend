5fbee76c7a5dcd2cff2210ac5620b7a5651fa061
beom <bdh3659@naver.com>
2025-01-13 16:31:10 +0900

    chore: Tag에 description 추가

--- src/components/common/Tag/index.tsx
+++ src/components/common/Tag/index.tsx
@@ -2,6 +2,13 @@ interface TagProps {
   children: string
 }
 
+/*
+  Tag
+  Description:
+    - 태그 컴포넌트
+    - children을 props로 받아서 사용
+*/
+
 const Tag = ({ children }: TagProps) => {
   return (
     <div className='font-medium h-10 p-3 inline-flex rounded-2xl items-center justify-center text-[#886296] bg-sub-color'>
