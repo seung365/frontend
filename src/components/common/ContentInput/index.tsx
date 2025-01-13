291d3371d1c2273e1991cf96331dc89af4c7ffe0
beom <bdh3659@naver.com>
2025-01-13 14:51:18 +0900

    feat: ContentInput 마크다운 입력 기능 추가

--- src/components/common/ContentInput/index.tsx
+++ src/components/common/ContentInput/index.tsx
@@ -1,22 +1,35 @@
+import MDEditor from '@uiw/react-md-editor'
+import '@uiw/react-md-editor/markdown-editor.css'
+import './mdeditor-styles.css'
+
 interface ContentInputProps {
   placeholder?: string
+  value?: string
+  onChange?: (value?: string) => void
 }
 
 /*
   ContentInput
   Description:
-    - 본문을 입력하는 textarea 컴포넌트
-    - placeholder를 props로 받아서 사용
+    - 본문을 입력하는 컴포넌트
+    - placeholder, value, onChange를 props로 받아서 사용
 */
 
 const ContentInput = ({
   placeholder = '본문을 입력해 주세요.',
+  value = '',
+  onChange,
 }: ContentInputProps) => {
   return (
     <div className='relative w-full h-auto max-w-4xl min-h-96'>
-      <textarea
-        className='w-full h-full p-6 text-xl font-semibold rounded-md resize-none cursor-text focus:outline-none'
-        placeholder={placeholder}
+      <MDEditor
+        value={value}
+        onChange={onChange}
+        textareaProps={{
+          placeholder: placeholder,
+        }}
+        preview='edit'
+        className='w-full min-h-96 custom-editor'
       />
     </div>
   )
