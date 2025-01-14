ee5ac768879a3e769b9236bc7d31e65306ee5f5b
min-s9709 <mim970929@naver.com>
2025-01-14 17:31:37 +0900

    feat: 프로필 카드 Link 연결

--- src/components/common/Card/ProfileCard/index.tsx
+++ src/components/common/Card/ProfileCard/index.tsx
@@ -1,4 +1,7 @@
+import { Link } from 'react-router-dom'
+
 interface ProfileCardProps {
+  profileId: string
   nickname: string
   profileImg: string
   about: string
@@ -9,21 +12,28 @@ ProfileCard 컴포넌트
 -사용자의 프로필을 보여주는 컴포넌트
 */
 
-const ProfileCard = ({ nickname, profileImg, about }: ProfileCardProps) => {
+const ProfileCard = ({
+  profileId,
+  nickname,
+  profileImg,
+  about,
+}: ProfileCardProps) => {
   return (
-    <section className='w-full min-h-72'>
-      <img
-        src={profileImg}
-        alt='프로필 이미지'
-        className='object-cover w-full cursor-pointer h-1/2 rounded-xl'
-      />
-      <div className='flex flex-col gap-2 p-2'>
-        <span className='text-main-black line-clamp-1'>{nickname}</span>
-        <p className='text-[14px] text-dark-gray line-clamp-3 min-h-16'>
-          {about}
-        </p>
-      </div>
-    </section>
+    <Link to={`/profile/${profileId}`}>
+      <section className='w-full min-h-72'>
+        <img
+          src={profileImg}
+          alt='프로필 이미지'
+          className='object-cover w-full cursor-pointer h-1/2 rounded-xl'
+        />
+        <div className='flex flex-col gap-2 p-2'>
+          <span className='text-main-black line-clamp-1'>{nickname}</span>
+          <p className='text-[14px] text-dark-gray line-clamp-3 min-h-16'>
+            {about}
+          </p>
+        </div>
+      </section>
+    </Link>
   )
 }
 
