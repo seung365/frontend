96d2f7026fab53c54431951ccc5828c7f459450e
min-s9709 <mim970929@naver.com>
2025-01-14 17:27:24 +0900

    fix: 프로필 mock data id 추가

--- src/mocks/profileList.ts
+++ src/mocks/profileList.ts
@@ -1,59 +1,69 @@
 export const profileList = [
   {
+    profileId: '1',
     nickname: 'CodeMaster',
     profileImg: 'https://placehold.co/600x400',
     about:
       'A passionate full-stack developer exploring new technologies.A passionate full-stack developer exploring new technologies.',
   },
   {
+    profileId: '2',
     nickname: 'DesignPro',
     profileImg: 'https://placehold.co/600x400',
     about:
       'UI/UX designer who loves crafting user-friendly interfaces.A passionate full-stack developer exploring new technologies.',
   },
   {
+    profileId: '3',
     nickname: 'ReactGuru',
     profileImg: 'https://placehold.co/600x400',
     about:
       'React developer building scalable web applications.A passionate full-stack developer exploring new technologies.',
   },
   {
+    profileId: '4',
     nickname: 'BackendNinja',
     profileImg: 'https://placehold.co/600x400',
     about:
       'Node.js enthusiast specializing in backend systems and APIs.A passionate full-stack developer exploring new technologies.',
   },
   {
+    profileId: '5',
     nickname: 'FrontendQueen',
     profileImg: 'https://placehold.co/600x400',
     about:
       'Frontend developer creating interactive and dynamic user experiences.A passionate full-stack developer exploring new technologies.',
   },
   {
+    profileId: '6',
     nickname: 'DataWizard',
     profileImg: 'https://placehold.co/600x400',
     about:
       'Data scientist uncovering insights from big data.A passionate full-stack developer exploring new technologies.',
   },
   {
+    profileId: '7',
     nickname: 'DevOpsHero',
     profileImg: 'https://placehold.co/600x400',
     about:
       'DevOps engineer automating deployments and ensuring scalability.A passionate full-stack developer exploring new technologies.',
   },
   {
+    profileId: '8',
     nickname: 'TechExplorer',
     profileImg: 'https://placehold.co/600x400',
     about:
       'Technology enthusiast always learning about emerging tools and trends.A passionate full-stack developer exploring new technologies.',
   },
   {
+    profileId: '9',
     nickname: 'MobileGenius',
     profileImg: 'https://placehold.co/600x400',
     about:
       'Mobile app developer focused on cross-platform solutions.A passionate full-stack developer exploring new technologies.',
   },
   {
+    profileId: '10',
     nickname: 'AIArchitect',
     profileImg: 'https://placehold.co/600x400',
     about:
