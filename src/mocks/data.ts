322ff4350a431affe2b7db0595b0f43b35df2bea
beom <bdh3659@naver.com>
2025-01-15 17:43:25 +0900

    chore: BoardDetail data 추가

--- src/mocks/data.ts
+++ src/mocks/data.ts
@@ -23,3 +23,39 @@ export const categories = [
   '프로젝트 구인',
   '경험 공유',
 ]
+
+export const BoardDetailData = {
+  title: '제목입니다',
+  content: `## 안녕하세요
+
+  **hi**
+    
+  처음 뵙겠습니다
+  
+  <img src="https://avatars.githubusercontent.com/u/74394824?v=4"/>`,
+
+  comments: [
+    {
+      id: 1,
+      profile_img: 'https://avatars.githubusercontent.com/u/74394824?v=4',
+      nickname: '김코딩',
+      content: '좋은 글 감사합니다',
+      date: '2021-09-23',
+    },
+    {
+      id: 2,
+      profile_img: 'https://avatars.githubusercontent.com/u/74394824?v=4',
+      nickname: '김코딩',
+      content: '좋은 글 감사합니다',
+      date: '2021-09-23',
+    },
+  ],
+  profile_img: 'https://avatars.githubusercontent.com/u/74394824?v=4',
+  up_cnt: 10,
+  view_cnt: 100,
+  data: '2021-09-23',
+  profile_id: '코딩좋아',
+  category: '자유게시판',
+  comment_cnt: 2,
+  tags: ['태그1', '태그2'],
+}
