44c14623ab1df5c34366b385a970cc1c50ce6bcc
beom <bdh3659@naver.com>
2025-01-16 16:57:41 +0900

    feat: Hub 페이지 구현 중

--- src/pages/Hub/index.tsx
+++ src/pages/Hub/index.tsx
@@ -1,5 +1,23 @@
+import { Filter, Grid, ProfileCard } from '../../components'
+import { profileList } from '../../mocks/profileList'
+
 const Hub = () => {
-  return <>허브</>
+  return (
+    <div className='flex flex-row gap-4 my-4'>
+      <Grid type='board'>
+        {profileList.map((profile) => (
+          <ProfileCard
+            key={profile.profileId}
+            profileId={profile.profileId}
+            profileImg={profile.profileImg}
+            nickname={profile.nickname}
+            about={profile.about}
+          />
+        ))}
+      </Grid>
+      <Filter />
+    </div>
+  )
 }
 
 export default Hub
