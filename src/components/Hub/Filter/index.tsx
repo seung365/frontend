3338da03c89c6440bbc53b91e06a504fd76d28f7
beom <bdh3659@naver.com>
2025-01-17 16:33:34 +0900

    feat: Hub Filter 컴포넌트 구현

--- src/components/Hub/Filter/index.tsx
+++ src/components/Hub/Filter/index.tsx
@@ -1,30 +1,57 @@
-import { SKILL_LIST } from '../../../constant'
+import { CAREER_TERM, SKILL_LIST } from '../../../constant'
+import { SortingType, TermType } from '../../../pages/Hub'
 import Button from '../../common/Button'
 
-const Filter = () => {
+interface FilterProps {
+  skills: string[]
+  onSorting: (newSorting: SortingType) => void
+  onSkills: (newSkill: string) => void
+  onTerm: (newTerm: TermType) => void
+}
+
+const Filter = ({ skills, onSkills, onSorting, onTerm }: FilterProps) => {
   return (
     <div className='border w-[300px] h-[1000px] rounded-xl'>
       <div className='flex flex-col gap-10 p-4 '>
         <div className='flex flex-col gap-2'>
           <span className='text-base font-bold'>정렬</span>
           <select className='w-full h-10 border rounded-lg border-dark-gray'>
-            <option value=''>최신순</option>
-            <option value=''>인기순</option>
+            <option value='' onClick={() => onSorting('최신순')}>
+              최신순
+            </option>
+            <option value='' onClick={() => onSorting('인기순')}>
+              인기순
+            </option>
           </select>
         </div>
         <div className='flex flex-wrap gap-2'>
           <span className='text-base font-bold'>기술 검색</span>
-          <input
-            type='text'
-            placeholder='검색어를 입력하세요'
-            className='w-full h-10 border rounded-lg border-dark-gray'
-          />
-          {SKILL_LIST.map((skill: string) => (
-            <Button theme='light'>{skill}</Button>
-          ))}
+          <div className='flex flex-wrap gap-3'>
+            {SKILL_LIST.map((skill: string) => (
+              <Button
+                key={skill}
+                theme={skills.includes(skill) ? 'dark' : 'light'}
+                onClick={() => onSkills(skill)}
+              >
+                {skill}
+              </Button>
+            ))}
+          </div>
         </div>
-        <div className='flex flex-col gap-10 p-4 '>
+        <div className='flex flex-col gap-2 p-4'>
           <span className='text-base font-bold'>재직 기간</span>
+          {CAREER_TERM.map((term: TermType, index: number) => (
+            <label key={index} className='flex items-center gap-2'>
+              <input
+                type='checkbox'
+                name='term'
+                value={term}
+                className='accent-purple-500'
+                onChange={() => onTerm(term)}
+              />
+              {term}
+            </label>
+          ))}
         </div>
       </div>
     </div>
