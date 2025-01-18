2cd06960b6fb1c4b6bcf8e514df782668f61e730
min-s9709 <mim970929@naver.com>
2025-01-18 17:15:30 +0900

    refactor: 프로필 메뉴 탭 리팩토링

--- src/components/Profile/ProfileMenuTab/index.tsx
+++ src/components/Profile/ProfileMenuTab/index.tsx
@@ -1,17 +1,11 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { twMerge } from 'tailwind-merge'
 import { PROFILE_MENU_TABS } from '../../../constant'
 
-interface ProfileMenuTabsProps {
-  pathname: string
-  queryString: string
-}
-
-export const ProfileMenuTab = ({
-  pathname,
-  queryString,
-}: ProfileMenuTabsProps) => {
-  const isActive = (path: string) => pathname + queryString === path
+export const ProfileMenuTab = () => {
+  const location = useLocation()
+  const isActive = (path: string) =>
+    location.pathname + location.search === path
 
   return (
     <aside className='min-w-64 h-fit border-r-[1px] border-light-gray'>
@@ -21,11 +15,14 @@ export const ProfileMenuTab = ({
             key={item.tab}
             className={twMerge(
               'px-4 py-2 text-lg font-semibold rounded-lg cursor-pointer text-board-dark-gray hover:bg-sub-color',
-              isActive(`${pathname}${item.query}`) &&
+              isActive(`${location.pathname}${item.query}`) &&
                 'bg-board-light-gray text-main-color',
             )}
           >
-            <Link to={`${pathname}${item.query}`} className='block w-full'>
+            <Link
+              to={`${location.pathname}${item.query}`}
+              className='block w-full'
+            >
               {item.tab}
             </Link>
           </li>
