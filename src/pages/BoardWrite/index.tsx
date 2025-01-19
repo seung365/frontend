df83e8664bf7cf697a8eb6a8602cfa675ae22226
beom <bdh3659@naver.com>
2025-01-19 11:00:43 +0900

    feat: BoardWrite에 카테고리별 기본 폼 제공 추가

--- src/pages/BoardWrite/index.tsx
+++ src/pages/BoardWrite/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import {
   Button,
@@ -8,22 +8,34 @@ import {
   Tag,
   TitleInput,
 } from '../../components'
+import { contentTemplates } from '../../constant'
 import { categories, tagName } from '../../mocks/data'
 
+export type CategoryType = (typeof categories)[number]
+
+// contentTemplates의 타입을 명시적으로 지정합니다
+
+// FormValues 인터페이스도 수정
 export interface FormValues {
   title: string
   content: string
-  category: string
+  category: CategoryType // string 대신 CategoryType 사용
   tags: string[]
 }
 
 const BoardWrite = () => {
   const { control, register, handleSubmit, setValue, watch } =
     useForm<FormValues>()
-
   const [open, setOpen] = useState(false)
-
   const selectedTags = watch('tags', [])
+  const selectedCategory = watch('category') // 선택된 카테고리 감시
+
+  // 카테고리가 변경될 때마다 템플릿 적용
+  useEffect(() => {
+    if (selectedCategory && contentTemplates[selectedCategory]) {
+      setValue('content', contentTemplates[selectedCategory])
+    }
+  }, [selectedCategory, setValue])
 
   const handleTagSelect = (tag: string) => {
     const currentTags = selectedTags || []
