6443cc587dbb77b917b278662ab1ab6534487681
min-s9709 <mim970929@naver.com>
2025-01-20 01:34:04 +0900

    feat: 프로필 정보 수정 진행중

--- src/components/Profile/ProfileInfo/index.tsx
+++ src/components/Profile/ProfileInfo/index.tsx
@@ -1,4 +1,6 @@
-import { Button, StatItem } from '../../../components'
+import { useState } from 'react'
+import EditIcon from '../../../assets/icons/edit.svg?react'
+import { Button, Modal, StatItem } from '../../../components'
 
 interface ProfileInfoProps {
   isMyProfile: boolean
@@ -13,6 +15,7 @@ interface ProfileInfoProps {
 /**
  * 프로필 정보 컴포넌트
  * isMyProfile 값에 따라 프로필 수정이나 팔로우하기 버튼 같은 요소 처리
+ * 프로필 수정은 모달로 작업할 예정.
  * @description
  */
 
@@ -27,6 +30,11 @@ const ProfileInfo = ({
   boardStatistics,
 }: ProfileInfoProps) => {
   console.log(boardStatistics)
+  const [isEditOpen, setIsEditOpen] = useState<boolean>(false)
+
+  const handleModalClose = () => {
+    setIsEditOpen(false)
+  }
   return (
     <section>
       <h1 className='mb-3 text-main-black text-size-title text-semibold'>
@@ -43,7 +51,20 @@ const ProfileInfo = ({
         </section>
 
         <section className='flex flex-grow-[7] flex-col gap-4 p-2'>
-          <h1 className='text-xl font-semibold text-main-black'>{nickName}</h1>
+          <section className='flex items-center gap-2'>
+            <h1 className='text-xl font-semibold text-main-black'>
+              {nickName}
+            </h1>
+            {isMyProfile && (
+              <button
+                className='border-[1px] border-main-color rounded-lg p-1'
+                onClick={() => setIsEditOpen(true)}
+              >
+                <EditIcon fill='#212529' />
+              </button>
+            )}
+          </section>
+
           <section className='flex gap-4'>
             <StatItem label='게시글' value={boardCnt} />
             <StatItem label='팔로워' value={followerCnt} />
@@ -54,6 +75,17 @@ const ProfileInfo = ({
           </p>
         </section>
       </section>
+      {isEditOpen && (
+        <Modal
+          isOpen={isEditOpen}
+          content={
+            <form>
+              <Button onClick={handleModalClose}>닫기</Button>
+            </form>
+          }
+          onClose={() => console.log(isEditOpen)}
+        />
+      )}
     </section>
   )
 }
