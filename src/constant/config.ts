4c1dae04b45d52f1cb8ff13db14110cc12c204b4
beom <bdh3659@naver.com>
2025-01-21 12:11:12 +0900

    chore: api config 추가

---
+++ src/constant/config.ts
@@ -0,0 +1,7 @@
+export const API_CONFIG = {
+  BASE_URL: import.meta.env.VITE_APP_BASE_URL,
+  AUTH: {
+    NAVER: import.meta.env.VITE_NAVER_AUTH_URL,
+    GITHUB: import.meta.env.VITE_GITHUB_AUTH_URL,
+  },
+} as const
