45180360ac7926e70ab3a0a5b5e4d3e9c506c2cf
beom <bdh3659@naver.com>
2025-01-22 18:10:10 +0900

    chore: useGetBoard 에서 useGetBoardDetail로 이름 변경

--- src/apis/board/useGetBoardDetail.tsx
+++ src/apis/board/useGetBoardDetail.tsx
@@ -2,21 +2,19 @@ import { useSuspenseQuery } from '@tanstack/react-query'
 import { BoardResponse } from '../../types/index'
 import { authInstance } from '../fetchInstance'
 
-const getBoard = async (
-  boardId: number | undefined,
-): Promise<BoardResponse> => {
+const getBoardDetail = async (boardId: number): Promise<BoardResponse> => {
   const response = await authInstance.get(`/board/${boardId}`)
   console.log('게시글 상세 응답:', response)
   return response.data
 }
 
-const useGetBoard = (id: string | undefined) => {
-  const boardId = id ? parseInt(id) : undefined
+const useGetBoardDetail = (id: string) => {
+  const boardId = parseInt(id)
   const { data } = useSuspenseQuery({
-    queryKey: ['board', boardId],
-    queryFn: () => getBoard(boardId),
+    queryKey: ['board', id],
+    queryFn: () => getBoardDetail(boardId),
   })
   return { data }
 }
 
-export default useGetBoard
+export default useGetBoardDetail
