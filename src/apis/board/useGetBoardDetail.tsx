78a4fc560f22dd5dbf11bbb82ffedddd63b5db03
beom <bdh3659@naver.com>
2025-01-23 14:31:36 +0900

    chore: boardId 타입 변경

--- src/apis/board/useGetBoardDetail.tsx
+++ src/apis/board/useGetBoardDetail.tsx
@@ -1,18 +1,17 @@
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { BoardResponse } from '../../types/index'
-import { authInstance } from '../fetchInstance'
+import { publicInstance } from '../fetchInstance'
 
-const getBoardDetail = async (boardId: number): Promise<BoardResponse> => {
-  const response = await authInstance.get(`/board/${boardId}`)
+const getBoardDetail = async (boardId: string): Promise<BoardResponse> => {
+  const response = await publicInstance.get(`/board/${boardId}`)
   console.log('게시글 상세 응답:', response)
   return response.data
 }
 
 const useGetBoardDetail = (id: string) => {
-  const boardId = parseInt(id)
   const { data } = useSuspenseQuery({
     queryKey: ['board', id],
-    queryFn: () => getBoardDetail(boardId),
+    queryFn: () => getBoardDetail(id),
   })
   return { data }
 }
