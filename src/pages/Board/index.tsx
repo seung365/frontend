f49382a43cb75d971a6a4fa0a982d0049152ec4a
min-s9709 <mim970929@naver.com>
2025-01-23 17:55:15 +0900

    feat: 게시판 페이지 api 연결 (카테고리별 조회, 검색, 태그 조회)

--- src/pages/Board/index.tsx
+++ src/pages/Board/index.tsx
@@ -1,82 +1,114 @@
-import { useForm } from 'react-hook-form'
-import { useLocation } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link, useLocation, useSearchParams } from 'react-router-dom'
+import useFetchBoardList from '../../apis/board/useFetchBoardList'
 import {
   BoardBanner,
   BoardCard,
   BoardCategoryTab,
+  BoardListSkeleton,
   BoardTagFilter,
+  ErrorComponent,
   Grid,
+  Loader,
+  NoResult,
   SearchBar,
 } from '../../components'
-import { boardPageBoardLit } from '../../mocks/boardList'
+import useTag from '../../hooks/useTag'
+import findCategoryId from '../../utils/findCategoryId'
 
-interface FormValues {
-  tags: string[]
-}
+/**
+ * TODO: 게시판 페이지 api 연결 작업 진행
+ * 전체 게시글 조회 o
+ * 카테고리별 게시글 조회 o
+ * 검색 기능 o
+ * 태그 선택으로 조회 o
+ * http://localhost:8080/api/board?tagNames=SPRING&tagNames=C&searchContents=%EB%98%91%EB%98%91%ED%95%9C%EA%B0%9C%EB%B0%9C%EC%9E%90&size=10&page=1
+ */
 
 const Board = () => {
   const location = useLocation()
-  const { setValue, watch } = useForm<FormValues>()
-
-  const selectedTags = watch('tags', [])
-
-  const handleTagSelect = (tag: string) => {
-    const currentTags = selectedTags || []
-    const isTagSelected = currentTags.includes(tag)
-
-    if (isTagSelected) {
-      setValue(
-        'tags',
-        currentTags.filter((currentTag: string) => currentTag !== tag),
-      )
-    } else {
-      setValue('tags', [...currentTags, tag])
-    }
-  }
-  console.log(selectedTags)
+  const categoryId = findCategoryId(location.pathname)
+  const [searchParams, setSearchParams] = useSearchParams()
+  const params = new URLSearchParams(searchParams)
+  const queryString = params.toString().replace('?', '$')
+  const { selectedTags, handleTagSelect, clearTags } = useTag({
+    params,
+    setSearchParams,
+  })
 
   const onSearch = (search?: string) => {
-    console.log(search)
+    if (search) {
+      params.set('searchContents', search)
+    } else {
+      params.delete('searchContents')
+    }
+    setSearchParams(params)
   }
 
+  useEffect(() => {
+    clearTags()
+  }, [location.pathname])
+
+  const { data, status, ref, isFetchingNextPage, hasNextPage } =
+    useFetchBoardList(categoryId, queryString)
+  console.log(data)
   return (
     <section className='flex flex-col w-full h-full gap-8 py-10'>
-      {/* 각 카테고리 탭 마다 다른 내용 나오게 할 예정 */}
       <BoardBanner pathname={location.pathname} />
-      {/* 카테고리 탭 */}
-      <BoardCategoryTab pathname={location.pathname} />
-      {/* 검색 및 필터링 */}
-      <section className='flex items-center justify-center w-full'>
-        <SearchBar onSearch={onSearch} />
+      <section className='flex items-center justify-center w-full h-8'>
+        <Link className='px-4 py-2 rounded-lg bg-main-color' to='/board/write'>
+          <span className='text-white'>게시글 작성하러 가기</span>
+        </Link>
       </section>
-      {/* 태그들의 리스트를 보여주고 선택하고 없애기*/}
+      <BoardCategoryTab pathname={location.pathname} />
+      <section className='flex items-center justify-center w-full'>
+        <SearchBar
+          params={params}
+          onSearchParams={setSearchParams}
+          onSearch={onSearch}
+          path={location.pathname}
+        />
+      </section>
       <BoardTagFilter
         selectedTags={selectedTags}
         onTagSelect={handleTagSelect}
       />
-      {/* 카테고리/검색/태그로 필터링 되는 게시글 리스트 -> 검색 api 나오면 추후에 컴포넌트로 분리 예정*/}
-      <section>
+      {status === 'pending' && <BoardListSkeleton type='board' />}
+      {status === 'error' && <ErrorComponent />}
+      {status === 'success' && (
         <Grid type='board'>
-          {boardPageBoardLit.map((item) => (
-            <BoardCard
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              content={item.content}
-              thumbnail={item.thumbnail}
-              categoryId={item.categoryId}
-              categoryName={item.categoryName}
-              date={item.createdAt}
-              upCnt={item.upCnt}
-              commentCnt={item.commentCnt}
-              viewCnt={item.viewCnt}
-              tags={item.tags}
-              profileImg={item.profileImg}
-              nickName={item.nickName}
-            />
+          {data?.pages.map((page, pageIndex) => (
+            <React.Fragment key={pageIndex}>
+              {page.content.map((board) => (
+                <BoardCard
+                  key={board.id}
+                  id={board.id}
+                  title={board.title}
+                  content={board.content}
+                  thumbnail={board.thumbnail}
+                  categoryId={board.categoryId}
+                  categoryName={board.categoryName}
+                  date={board.createdAt}
+                  upCnt={board.upCnt}
+                  commentCnt={board.commentCnt}
+                  viewCnt={board.viewCnt}
+                  profileImg={board.profileImage}
+                  nickName={board.nickName}
+                  tags={board.tag}
+                />
+              ))}
+            </React.Fragment>
           ))}
         </Grid>
-      </section>
+      )}
+      {data?.pages[0].empty && <NoResult />}
+      {hasNextPage && (
+        <div ref={ref}>
+          <section className='flex items-center justify-center w-full mt-3'>
+            {isFetchingNextPage ? <Loader /> : null}
+          </section>
+        </div>
+      )}
     </section>
   )
 }
