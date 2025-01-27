7d40c90ea90fc78783a86342dfd121bebeecb996
min-s9709 <mim970929@naver.com>
2025-01-27 18:05:12 +0900

    feat: 로그인을 전역상태 관리하기 위한 AuthStore 추가

---
+++ src/store/AuthStore.ts
@@ -0,0 +1,17 @@
+import { create } from 'zustand'
+
+type AuthState = {
+  isLogin: boolean
+  accessToken: string | null
+  memberId: string | null
+  setLogin: (token: string, memberId: string) => void
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  isLogin: false,
+  accessToken: null,
+  memberId: null,
+  setLogin: (accessToken, memberId) => {
+    set({ isLogin: true, accessToken, memberId })
+  },
+}))
