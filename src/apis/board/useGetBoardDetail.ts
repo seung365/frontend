fb0df19af267c21632985d74b248852d716fa2bc
beom <bdh3659@naver.com>
2025-02-01 17:18:29 +0900

    fix: getBoardDetail api 수정

--- src/apis/board/useGetBoardDetail.ts
+++ src/apis/board/useGetBoardDetail.ts
@@ -1,10 +1,10 @@
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { API_ROUTES } from '../../constant/api'
 import { BoardResponse } from '../../types/index'
-import { publicInstance } from '../fetchInstance'
+import { authInstance } from '../fetchInstance'
 
 const getBoardDetail = async (boardId: string): Promise<BoardResponse> => {
-  const response = await publicInstance.get(`/${API_ROUTES.BOARDS}/${boardId}`)
+  const response = await authInstance.get(`/${API_ROUTES.BOARDS}/${boardId}`)
   console.log('게시글 상세 응답:', response)
   return response.data
 }
