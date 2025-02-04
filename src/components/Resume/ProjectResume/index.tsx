d9fd3496958cc6b47d3dbec3ca1f945e6d2970dd
min-s9709 <mim970929@naver.com>
2025-02-04 19:47:32 +0900

    feat: 이력서 프로젝트 관련 api 연결 및 처리 로직 추가

--- src/components/Resume/ProjectResume/index.tsx
+++ src/components/Resume/ProjectResume/index.tsx
@@ -1,9 +1,12 @@
+import { useState } from 'react'
 import { Control, useFieldArray, UseFormRegister } from 'react-hook-form'
-import { Button, ProjectForm } from '../../../components'
+import useDeleteProject from '../../../apis/resume/project/useDeleteProject'
+import { Button, ProjectDetails, ProjectForm } from '../../../components'
 import { Project, UserResume } from '../../../types'
 
 interface ProjectResumeProps {
   register: UseFormRegister<UserResume>
+  watchedData: Project[]
   control: Control<UserResume>
   getValues: (name: string) => Project[]
   onProjectsSubmit: (data: Project[]) => void
@@ -14,6 +17,7 @@ const ProjectResume = ({
   register,
   getValues,
   onProjectsSubmit,
+  watchedData,
 }: ProjectResumeProps) => {
   const {
     fields: projectFields,
@@ -24,47 +28,88 @@ const ProjectResume = ({
     name: 'projects',
   })
 
+  const [isEdit, setIsEdit] = useState<boolean>(false)
+
+  const { mutate: deleteProject } = useDeleteProject()
+
   const handleSubmitAllProjects = () => {
     const projectsData = getValues('projects')
     onProjectsSubmit(projectsData)
+    setIsEdit(false)
+  }
+
+  const handleDelete = (id: number) => {
+    deleteProject({ id })
   }
 
   return (
-    <section className='flex flex-col w-full gap-3 '>
-      {projectFields.map((field, index) => (
-        <ProjectForm
-          key={field.id}
-          index={index}
-          register={register}
-          onRemove={() => removeProject(index)}
-        />
-      ))}
-      <div className='flex flex-col items-center w-full gap-4'>
-        {projectFields.length !== 0 && (
-          <Button
-            style={{ width: '20%' }}
-            type='button'
-            onClick={handleSubmitAllProjects}
-          >
-            작성
-          </Button>
+    <section className='flex flex-col w-full gap-3'>
+      {projectFields.map((field, index) =>
+        isEdit ? (
+          <ProjectForm
+            key={field.id}
+            index={index}
+            register={register}
+            onRemove={() => removeProject(index)}
+          />
+        ) : (
+          <ProjectDetails
+            key={field.id}
+            project={watchedData[index]}
+            onDelete={handleDelete}
+          />
+        ),
+      )}
+      <div className='flex justify-center gap-4 p-3'>
+        {isEdit ? (
+          <div className='flex gap-4'>
+            {projectFields.length !== 0 && (
+              <Button onClick={handleSubmitAllProjects}>저장하기</Button>
+            )}
+
+            <Button
+              type='button'
+              onClick={() =>
+                appendProject({
+                  projectName: '',
+                  description: '',
+                  organization: '',
+                  startDate: '',
+                  endDate: '',
+                  link: '',
+                })
+              }
+              className='bg-dark-gray'
+            >
+              프로젝트 추가
+            </Button>
+          </div>
+        ) : (
+          <>
+            {watchedData.length !== 0 ? (
+              <Button type='button' onClick={() => setIsEdit(!isEdit)}>
+                수정하기
+              </Button>
+            ) : (
+              <Button
+                type='button'
+                onClick={() => {
+                  setIsEdit(true)
+                  appendProject({
+                    projectName: '',
+                    description: '',
+                    organization: '',
+                    startDate: '',
+                    endDate: '',
+                    link: '',
+                  })
+                }}
+              >
+                프로젝트 작성하기
+              </Button>
+            )}
+          </>
         )}
-        <button
-          type='button'
-          onClick={() =>
-            appendProject({
-              name: '',
-              description: '',
-              organization: '',
-              startDate: '',
-              endDate: '',
-              link: '',
-            })
-          }
-          className='w-1/5 px-4 py-2 mx-auto text-white rounded-lg bg-dark-gray'
-        >
-          프로젝트 추가
-        </button>
       </div>
     </section>
   )
