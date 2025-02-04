66ed4d3ce9b6aa5af80ee62c4b3ad7108660b1a7
beom <bdh3659@naver.com>
2025-02-05 02:57:39 +0900

    feat: useGetHubList API 백엔드와 연결

--- src/apis/hub/useGetHubList.ts
+++ src/apis/hub/useGetHubList.ts
@@ -1,5 +1,32 @@
 import { useInfiniteQuery } from '@tanstack/react-query'
-import profileList from '../../mocks/profileList'
+import { API_ROUTES } from '../../constant/api'
+import { publicInstance } from '../fetchInstance'
+
+interface Profile {
+  profileId: string
+  nickname: string
+  about: string
+  profileImage: string
+  employmentPeriod: string
+  skills: string[]
+}
+
+interface PageResponse {
+  content: Profile[]
+  totalElements: number
+  totalPages: number
+  size: number
+  number: number
+  first: boolean
+  last: boolean
+  numberOfElements: number
+  empty: boolean
+  pageable: {
+    offset: number
+    pageSize: number
+    pageNumber: number
+  }
+}
 
 const userkeys = {
   all: ['users'] as const,
@@ -12,44 +39,41 @@ const userkeys = {
 interface getHubListProps {
   size?: number
   pageParam?: number
+  queryString: string
 }
 
-// const getHubList = async ({ size = 10, pageParam = 0 }: getHubListProps) => {
-//   const response = await publicInstance.get(
-//     `/${API_ROUTES.HUBS}?size=${size}&page=${pageParam}`,
-//   )
-//   return response.data
-// }
-
-const getMockHubList = async ({ size = 9, pageParam = 0 }: getHubListProps) => {
-  const start = pageParam * size
-  const end = start + size
-
-  await new Promise((resolve) => setTimeout(resolve, 500))
-
-  return {
-    data: {
-      contents: profileList.slice(start, end),
-      totalPages: Math.ceil(profileList.length / size),
-      lastPage: end >= profileList.length,
-    },
-  }
+const getHubList = async ({
+  size = 9,
+  pageParam = 0,
+  queryString = '',
+}: getHubListProps): Promise<PageResponse> => {
+  const response = await publicInstance.get(
+    `/${API_ROUTES.HUB}?size=${size}&page=${pageParam}&${queryString}`,
+  )
+  return response.data
 }
 
-const useGetHubList = () => {
+const useGetHubList = (queryString: string) => {
   const { data, hasNextPage, status, fetchNextPage, isFetchingNextPage } =
     useInfiniteQuery({
-      queryKey: userkeys.lists(),
-      queryFn: ({ pageParam = 0 }) => getMockHubList({ size: 9, pageParam }),
-      getNextPageParam: (data, pages) => {
-        if (data.data.lastPage) {
+      queryKey: [...userkeys.lists(), queryString],
+      queryFn: ({ pageParam = 0 }) =>
+        getHubList({ size: 9, pageParam, queryString }),
+      getNextPageParam: (data) => {
+        if (data.last) {
           return undefined
         }
-        return pages.length + 1
+        return data.number + 1
       },
       initialPageParam: 0,
     })
-  return { data, hasNextPage, status, isFetchingNextPage, fetchNextPage }
+
+  const contents =
+    data?.pages.reduce<Profile[]>((acc, page) => {
+      return [...acc, ...page.content]
+    }, []) ?? []
+
+  return { contents, hasNextPage, status, isFetchingNextPage, fetchNextPage }
 }
 
 export default useGetHubList
