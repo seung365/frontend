3dba3c0a18878ac00319209a881bd7f8746c2977
beom <bdh3659@naver.com>
2025-02-04 13:49:16 +0900

    feat: FloatingButton 구현

--- src/components/Chat/FloatingButton/index.tsx
+++ src/components/Chat/FloatingButton/index.tsx
@@ -1,8 +1,19 @@
+import { useChatStore } from '../../../store/ChatStore'
+import ChatModal from '../ChatModal'
+
 const FloatingButton = () => {
+  const { isOpen, setIsOpen } = useChatStore()
+
   return (
-    <button className='fixed flex items-center justify-center rounded-full w-14 h-14 text-main-color bg-sub-color right-10 bottom-10'>
-      Chat
-    </button>
+    <div>
+      <button
+        className='fixed flex items-center justify-center rounded-full w-14 h-14 text-main-color bg-sub-color right-10 bottom-10'
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        Chat
+      </button>
+      {isOpen && <ChatModal />}
+    </div>
   )
 }
 
