aaeba7bad186626bf3d8199f7be78cee5578c7cf
beom <bdh3659@naver.com>
2025-02-04 19:57:00 +0900

    style: HubListSkeleton 간격 수정

--- src/components/common/Skeleton/HubListSkeleton/index.tsx
+++ src/components/common/Skeleton/HubListSkeleton/index.tsx
@@ -5,10 +5,10 @@ const HubListSkeleton = ({ isInitialLoading = false }) => {
   const skeletonCount = isInitialLoading ? 9 : 3
 
   return (
-    <div className='grid w-full grid-cols-3 gap-4'>
+    <div className='grid w-full grid-cols-3 gap-9'>
       {Array.from({ length: skeletonCount }).map((_, i) => {
         return (
-          <section key={i} className='w-full mt-4 mb-4 min-h-50 rounded-xl'>
+          <section key={i} className='w-full mb-4 min-h-50 rounded-xl'>
             <Skeleton
               className='object-cover w-full h-1/2 rounded-xl'
               height='60%'
