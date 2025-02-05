83c0143a1c02cd0e27503427710d72e9be659a1e
min-s9709 <mim970929@naver.com>
2025-02-05 20:32:24 +0900

    add: protected routes path 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,23 @@ import { useEffect } from 'react'
 import usePostToken from './apis/member/usePostToken.ts'
 import queryClient from './apis/queryClient'
 import { Routes } from './routes'
+import { PROTECTED_ROUTES } from './routes/path.ts'
 import { useAuthStore } from './store/AuthStore.ts'
 import './styles/global.css'
 
 const App = () => {
   const isLogin = useAuthStore((state) => state.isLogin)
+  const pathname = window.location.pathname
   const postToken = usePostToken()
   useEffect(() => {
     const checkAuth = async () => {
-      if (!isLogin) {
+      if (!isLogin && !PROTECTED_ROUTES.includes(pathname)) {
         await postToken()
       }
     }
 
     checkAuth()
-  }, [isLogin, postToken])
+  }, [isLogin, postToken, pathname])
 
   return (
     <QueryClientProvider client={queryClient}>
