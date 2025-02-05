902231652851291ac7e987a53705f81922abea82
min-s9709 <mim970929@naver.com>
2025-02-05 14:44:27 +0900

    feat: axios intercetopr로 로그인 관련 error 로직 처리

--- src/apis/fetchInstance.ts
+++ src/apis/fetchInstance.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 import { API_CONFIG } from '../constant/config'
 import { useAuthStore } from '../store/AuthStore'
 
@@ -18,6 +19,21 @@ authInstance.interceptors.request.use((config) => {
   }
   return config
 })
+
+authInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (axios.isAxiosError(error) && error.response?.status === 401) {
+      const navigate = useNavigate()
+      console.error('401 Unauthorized - Access Token 만료 또는 인증 실패')
+      alert('세션이 만료되었습니다. 다시 로그인 해주세요.')
+      navigate('/signin')
+
+      useAuthStore.getState().setLogout()
+    }
+    return Promise.reject(error)
+  },
+)
 
 const publicInstance = axios.create({
   baseURL: API_CONFIG.BASE_URL,
