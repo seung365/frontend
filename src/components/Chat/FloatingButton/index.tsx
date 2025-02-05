4eb44f29fe72438245ca4a5bb41318f7d0190dbd
beom <bdh3659@naver.com>
2025-02-05 20:07:41 +0900

    style: FloatingButton UI 수정

--- src/components/Chat/FloatingButton/index.tsx
+++ src/components/Chat/FloatingButton/index.tsx
@@ -5,9 +5,9 @@ const FloatingButton = () => {
   const { isOpen, setIsOpen } = useChatStore()
 
   return (
-    <div>
+    <div className='fixed z-50 flex items-center justify-center right-10 bottom-10'>
       <button
-        className='fixed flex items-center justify-center text-white transition-shadow duration-300 rounded-full shadow-lg w-14 h-14 bg-main-color right-10 bottom-10 hover:shadow-xl'
+        className='flex items-center justify-center text-white transition-shadow duration-300 rounded-full shadow-lg w-14 h-14 bg-main-color hover:shadow-xl'
         onClick={() => setIsOpen(!isOpen)}
       >
         Chat
