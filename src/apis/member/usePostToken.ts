68687f711d69ba611a0da57327028ec43903d7e2
min-s9709 <mim970929@naver.com>
2025-02-05 20:32:41 +0900

    fix: console.log 제거

--- src/apis/member/usePostToken.ts
+++ src/apis/member/usePostToken.ts
@@ -6,12 +6,8 @@ const usePostToken = () => {
 
   const postToken = async (): Promise<void> => {
     try {
-      console.log('현재 쿠키:', document.cookie)
-
       const response = await publicInstance.post<string>('/reissue')
-      console.log('성공 응답:', response)
       const accessToken = response.headers.access
-      console.log('발급된 토큰:', accessToken)
       const memberId = response.data
       if (accessToken && memberId) {
         setLogin(accessToken, memberId)
